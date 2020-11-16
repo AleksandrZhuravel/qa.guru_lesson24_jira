6f0710161e291ef8d8a76d83578d376bd30b3b77
AleksandrZhuravel <cah40c@tut.by>
2020-11-16 15:45:08 +0300

    auxiliary java-files added

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,44 @@
+package tests;
+
+import config.ConfigHelper;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
+import static helpers.BrowserstackHelper.getBSPublicLink;
+import static helpers.DriverHelper.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+public class TestBase {
+
+    @BeforeAll
+    @Step("Tests setup")
+    public static void beforeAll() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        configureSelenide();
+    }
+
+    @AfterEach
+    @Step("Attachments")
+    public void afterEach(){
+        String sessionId = getSessionId();
+
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+//        attachNetwork(); // todo
+        if (ConfigHelper.isWeb())
+            attachAsText("Browser console logs", getConsoleLogs());
+        if (ConfigHelper.isAndroid() || ConfigHelper.isIos())
+            attachAsText("Browserstack build link", getBSPublicLink(sessionId));
+
+        closeWebDriver();
+
+        if (!ConfigHelper.getWebVideoStorage().equals(""))
+            attachVideo(sessionId); // in browserstack video url generates after driver close
+    }
+}
